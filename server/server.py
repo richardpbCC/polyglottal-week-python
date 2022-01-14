1968731988cb345661b9677716a851aabaf3403c
richardpb <rpbeavis@gmail.com>
2022-01-14 15:09:38 +0900

    fix: remove message to client due to new line charachter not working in f string statement

--- server/server.py
+++ server/server.py
@@ -10,7 +10,7 @@ server.bind((HOST, PORT))
 server.listen()
 
 clients = []
-nicknames = []
+usernames = []
 
 
 # broadcast
@@ -26,15 +26,15 @@ def handle(client):
     while True:
         try:
             message = client.recv(1024)
-            print(f"{nicknames[clients.index(client)]} says {message}")
+            print(f"{usernames[clients.index(client)]} says {message}")
             broadcast(message)
         except:
             index = clients.index(client)
-            nickname = nicknames[index]
-            print(f"{nickname} has disconnected") 
+            username = usernames[index]
+            print(f"{username} has disconnected") 
             clients.remove(client)            
             client.close()            
-            nicknames.remove(nickname)
+            usernames.remove(username)
             break
 
 
@@ -46,14 +46,14 @@ def receive():
         print(f"Connected with {str(address)}")
 
         client.send('request to connect'.encode('utf-8'))
-        nickname = client.recv(1024)
+        username = client.recv(1024)
 
-        nicknames.append(nickname)
+        usernames.append(username)
         clients.append(client)
-
-        print(f"Nickname of the client is {nickname}\n".encode('utf-8'))
-        broadcast(f"{nickname} connected to the server\n".encode('utf-8'))
-        client.send("Connected to the server".encode('utf-8'))
+        
+        print(f"Username of the client is {username}".encode('utf-8'))
+        # broadcast(f"{username} connected to the server".encode('utf-8'))
+        # client.send("Connected to the server".encode('utf-8'))
 
         thread = threading.Thread(target=handle, args=(client,))
         thread.start()
